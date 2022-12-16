ea4aabd3bfe50955535d4f7138cfbe73c32357a3
francescotili <francesco.tili@gmail.com>
2022-12-16 21:30:11 +0100

    Customized buttons for simulation

---
+++ src/app/graphics/buttons/buttons.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'simulation-buttons',
+  templateUrl: './buttons.component.html',
+  styleUrls: ['./buttons.component.css'],
+})
+export class SimulationButtonsComponent {
+  @Input() viewBtn: {
+    start: boolean;
+    pause: boolean;
+    reset: boolean;
+    resume: boolean;
+  };
+  @Input() time: string;
+  @Output() clickStart = new EventEmitter<string>();
+  @Output() clickPause = new EventEmitter<string>();
+  @Output() clickResume = new EventEmitter<string>();
+  @Output() clickReset = new EventEmitter<string>();
+
+  simulationStart() {
+    this.clickStart.emit();
+  }
+
+  simulationPause() {
+    this.clickPause.emit();
+  }
+
+  simulationResume() {
+    this.clickResume.emit();
+  }
+
+  simulationReset() {
+    this.clickReset.emit();
+  }
+}
